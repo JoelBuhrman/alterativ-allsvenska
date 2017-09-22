950f3f65655ae68779c2dbccc96b0afea17ba565
Joel Buhrman <joel.buhrman@live.se>
2017-09-22 11:01:43 +0200

    Put stats in right order and made diff clearer

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -5,8 +5,8 @@ export default class Stats extends Component{
   render(){
     return(
       <div>
-        <div style={{marginLeft: '60%'}} className="diff titles">D</div>
-        <div className="games titles">S</div>
+        <div style={{marginLeft: '60%'}}className="games titles">S</div>
+        <div className="diff titles">+/-D</div>
         <div className="points titles">P</div>
         <div className="average titles">AVG</div>
       </div>
